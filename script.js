b855a04b68bf19acc63316a9e50287d81f7c25c3
Arnab-1473 <arban.nilr.1473@gmail.com>
2023-03-02 14:07:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 //your JS code here. If required.
 function backspace(){
-    let myinput = document.getElementById("display").value;
-    let ans = myinput.substring(0,myinput.length-1);
+    let myinput = document.getElementById("display");
+    let ans = myinput.value.substring(0,myinput.value.length-1);
     console.log(ans);
-    document.getElementById("display").value = ans;
+    // document.getElementById("display").value = ans;
+    console.log(myinput);
+    myinput.value = ans;
 }
